69abb59b4e0d18e81b17906914a6ba9d5c9d851a
Yosef <yosefmel@post.bgu.ac.il>
2018-06-10 23:26:34 +0300

    register with form check, problem with the drop down list

--- routing/components/Welcome/registerController.js
+++ routing/components/Welcome/registerController.js
@@ -9,12 +9,12 @@ var reg=   angular.module('citiesApp');
     // self.categories=["Sights & Landmraks" ,"Outdoor Activities ", "Museums" ,"Shopping" ,"Nightlife"];
 let serverUrl = 'http://localhost:8080/'
         // register user
-        
+
         $http.get(serverUrl + "countries")
             .then(function (response) {
                let res=response.data;
                 for (var x=0; x<res.length; x++) {
-                    var str=res[x]['Name']['0']; 
+                    var str=res[x]['Name']['0'];
                     self.countries[x]=str;
                 }
                 console.log(self.countries);
@@ -23,13 +23,18 @@ let serverUrl = 'http://localhost:8080/'
                 //Second function handles error
                 console.log("error");
             });
-      
-            self.reg = function () {
+
+            self.reg = function (bool){
+              if (bool==false) {
+                $("input.ng-invalid").css({border: "2px solid red"});
+                alert('Please fill out all fields marked with red');
+                return;
+              }
                 //json objects to send
                 //register user fields
-                //NEED TO ADD CATEGORIES 
-                myObj = { 
-                    "FirstName":self.firstName, 
+                //NEED TO ADD CATEGORIES
+                myObj = {
+                    "FirstName":self.firstName,
                     "LastName":self.lastName,
                     "City":self.City,
                     "Username":self.userName ,
@@ -37,23 +42,37 @@ let serverUrl = 'http://localhost:8080/'
                     "Email":self.email ,
                     "Questions":self.ver1 ,
                     "Verifiers":self.ans1 ,
-                    "Questions":self.ver2, 
+                    "Questions":self.ver2,
                     "Verifiers":self.ans2,
-                    "Country":self.Country, 
+                    "Country":self.Country,
                     "Categories":[]
-                    }     
+                    }
                     for(let i=0;i<self.categories.length;i++){
                         if(self.categories[i].selected === "YES"){
-                            myObj.Categories.push(self.categories[i].category);               
-                        }               
+                            myObj.Categories.push(self.categories[i].category);
+                        }
                     }
-                //register user
-                $http.post(serverUrl + "register", myObj)
+                // //register user
+                 $http.post(serverUrl + "register", myObj)
 
-                    .then(function (response) {
-                        console.log(response);
-                    }, function (response) {
-                         alert("something went wrong");
-                     });   
+                     .then(function (response) {
+                         console.log(response);
+                     }, function (response) {
+                          alert("something went wrong");
+                      });
             }
+                //validation check
+                $scope.handlePatternPassword = (function() {
+                    var regex = /^[A-Za-z0-9!@#$%^&*()_]{4,20}$/;
+                    return {
+                      test: function(value) {
+                        if ($scope.user.isLogged) {
+                          return (value.length > 0) ? regex.test(value) : true;
+                        } else {
+                          return regex.test(value);
+                        }
+                      }
+                    };
+                  })();
+
         }]);
