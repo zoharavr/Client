6a630a29f6f6ee2b677dd958bdb2bd8b0d15d5a4
Zohar <zoharavr@post.bgu.ac.il>
2018-06-11 18:21:00 +0300

    added filtering : order by views,rating and alphbatic order + serch by point name + filter by category

--- routing/components/POI/allPointCtrl.js
+++ routing/components/POI/allPointCtrl.js
@@ -2,6 +2,8 @@ angular.module('citiesApp')
     .controller('allPointsController', ['$http', function ($http) {
         let serverUrl = 'http://localhost:8080/';
         self = this;
+        self.order=["Views","Ratings","PointName"];
+        self.categories=["Sights & Landmraks" ,"Outdoor Activities ", "Museums" ,"Shopping" ,"Nightlife"];
    //need to happen imediatlly 
             $http.get(serverUrl + "Points")
                 .then(function (response) {
