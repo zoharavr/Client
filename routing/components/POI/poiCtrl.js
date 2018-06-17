9d1df31ec898c87f8d6a49c2894f1fb7f451a569
yosef <yosefmel@post.bgu.ac.il>
2018-06-18 00:57:56 +0300

    map beta version

--- routing/components/POI/poiCtrl.js
+++ routing/components/POI/poiCtrl.js
@@ -12,10 +12,21 @@ mainApp.factory('setID', ['localStorageService', function (localStorageService) 
     return factory;
 }]);
 mainApp.controller('poiCtrl', ['$http', 'setID', 'localStorageService', function ($http, setID,localStorageService ) {
+    
+    
     let serverUrl = 'http://localhost:8080/';
     self = this;
  // check if the user is logged in if so show the comment button
     let token =  localStorageService.get('token');
+    var mymap = L.map('mapid').setView([10, 60], 13);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoiem9oYXJhdnIiLCJhIjoiY2ppaGc5NDFtMTVzcTN3dXBnMGVieWl0ciJ9.11y3xDJ-X2_qezzQB_uOYQ', {
+      maxZoom: 18,
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      id: 'mapbox.streets'
+    }).addTo(mymap);
+ 
     if(token != null){
         self.flag=true
     }
@@ -37,7 +48,10 @@ mainApp.controller('poiCtrl', ['$http', 'setID', 'localStorageService', function
         }, (response) => {
             console.log(response);
         });
-    //user can make a comment about specific poi only once
+
+  
+
+        //user can make a comment about specific poi only once
     self.make_comment = function () {
         let comment = {
             Comment: self.content,
